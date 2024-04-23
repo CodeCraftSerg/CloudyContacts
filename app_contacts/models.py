f9feecab83ac119a60608dd77ccda6657e97003e
shuguruiua <popov.yk@gmail.com>
2024-04-23 10:49:53 +0300

    added models

--- app_contacts/models.py
+++ app_contacts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=20, unique=False,  default="Name", null=False)
+    surname = models.CharField(max_length=20, unique=False,  default="Surname", null=False)
+    email = models.EmailField(unique=True, null=True)
+    mobile_phone = models.CharField(max_length=20, null=True, unique=True)
+    work_phone = models.CharField(max_length=20, null=True, unique=True)
+    home_phone = models.CharField(max_length=20, null=True, unique=False)
+    birthdate = models.DateField(null=True, blank=True)
+    is_favorite = models.BooleanField(default=False)
+    facebook = models.URLField(null=True, blank=True)
+    instagram = models.URLField(null=True, blank=True)
+    tiktok = models.URLField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f"{self.name} {self.surname}"
--- app_notes/models.py
+++ app_notes/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+from app_contacts.models import Contact
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=20)
+    body = models.TextField()
+    date = models.DateField(null=True, blank=True)
+    is_done = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+
+    contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tag = models.ManyToManyField("Tag")
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20, null=False, unique=True)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
