a9e049bff9d7a38ed5420c862f2481dc74e2e9c5
Olha <142577158+HelgaTsar@users.noreply.github.com>
2024-05-01 12:30:54 +0300

    News

--- app_news/urls.py
+++ app_news/urls.py
@@ -6,4 +6,7 @@ app_name = "app_news"
 
 urlpatterns = [
     path("", views.main, name="news_page"),
+    path('sport-news/', views.sport_news, name='sport_news'),
+    path('politic-news/', views.politic_news, name='politic_news'),
+    path('culture-news/', views.culture_news, name='culture_news'),
 ]
