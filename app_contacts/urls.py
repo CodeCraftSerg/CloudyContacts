70743cd7398cdbb543c2232fd86793f973ecaeb4
shuguruiua <popov.yk@gmail.com>
2024-05-02 15:31:14 +0300

    Contact management: added pagination for contacts and search result add contact - updated: added navigation update contact - updated: added navigation delete contact - done contact details - updated: added search by name, surname, address, email, phone numbers
    Bug fixed:
    contact_update.html tamplate raised error 500
    contact_update.html socials not updated in right way

--- app_contacts/urls.py
+++ app_contacts/urls.py
@@ -6,6 +6,7 @@ app_name = "app_contacts"
 
 urlpatterns = [
     path("", views.main, name="contacts"),
+    path("<int:page>", views.main, name="index_paginate"),
     path("add_contact/", views.add_contact, name="add_contact"),
     path("contact_details/<int:contact_id>/", views.contact_details, name="contact_details"),
     path("delete_contact/<int:contact_id>/", views.delete_contact, name="delete_contact"),
