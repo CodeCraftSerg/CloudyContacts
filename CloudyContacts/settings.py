d785d01f5e47ad90c9a08d12d7e4c027bc414e4e
Serg <srg.first@gmail.com>
2024-05-04 19:40:06 +0300

    ver_0.0.24 (update db connection)

--- CloudyContacts/settings.py
+++ CloudyContacts/settings.py
@@ -36,7 +36,7 @@ DEBUG = False
 ALLOWED_HOSTS = [
     "DJANGO_ALLOWED_HOSTS",
     "127.0.0.1",
-    "cloudy-contacts-grey-1bd53251.koyeb.app/",
+    "cloudy-contacts-grey-50db466b.koyeb.app",
 ]
 # ALLOWED_HOSTS = os.getenv(
 #     "DJANGO_ALLOWED_HOSTS",
@@ -44,7 +44,7 @@ ALLOWED_HOSTS = [
 #     "https://cloudy-contacts-grey-1bd53251.koyeb.app/",
 # ).split(",")
 
-CSRF_TRUSTED_ORIGINS = ["https://cloudy-contacts-grey-1bd53251.koyeb.app/"]
+CSRF_TRUSTED_ORIGINS = ["https://cloudy-contacts-grey-50db466b.koyeb.app"]
 # CSRF_TRUSTED_ORIGINS = os.getenv(
 #     "https://cloudy-contacts-grey-1bd53251.koyeb.app/"
 # ).split(",")
@@ -122,27 +122,27 @@ WSGI_APPLICATION = "CloudyContacts.wsgi.application"
 # }
 
 # --- koyeb Database ---
-DATABASES = {
-    "default": {
-        "ENGINE": env("KOYEB_ENGINE"),
-        "NAME": env("KOYEB_NAME"),
-        "USER": env("KOYEB_USER"),
-        "PASSWORD": env("KOYEB_PASSWORD"),
-        "HOST": env("KOYEB_HOST"),
-    }
-}
-
-# --- ElephantSQL Database ---
 # DATABASES = {
 #     "default": {
-#         "ENGINE": env("ELEPHANT_ENGINE"),
-#         "NAME": env("ELEPHANT_DB_NAME_USER"),
-#         "USER": env("ELEPHANT_DB_NAME_USER"),
-#         "PASSWORD": env("ELEPHANT_PASSWORD"),
-#         "HOST": env("ELEPHANT_HOST"),
-#         "PORT": env("ELEPHANT_PORT"),
+#         "ENGINE": env("KOYEB_ENGINE"),
+#         "NAME": env("KOYEB_NAME"),
+#         "USER": env("KOYEB_USER"),
+#         "PASSWORD": env("KOYEB_PASSWORD"),
+#         "HOST": env("KOYEB_HOST"),
 #     }
 # }
+
+# --- ElephantSQL Database ---
+DATABASES = {
+    "default": {
+        "ENGINE": env("ELEPHANT_ENGINE"),
+        "NAME": env("ELEPHANT_DB_NAME_USER"),
+        "USER": env("ELEPHANT_DB_NAME_USER"),
+        "PASSWORD": env("ELEPHANT_PASSWORD"),
+        "HOST": env("ELEPHANT_HOST"),
+        "PORT": env("ELEPHANT_PORT"),
+    }
+}
 
 
 # Password validation
