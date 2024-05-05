7a1c4f79fd193d7a4a3d915382e41a451025b31c
Serg <srg.first@gmail.com>
2024-05-05 15:42:05 +0300

    ver_0.1.1 (attempts to deploy)

--- CloudyContacts/settings.py
+++ CloudyContacts/settings.py
@@ -30,9 +30,9 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DEBUG")
+# DEBUG = env("DEBUG")
 # DEBUG = False
-# DEBUG = True
+DEBUG = True
 
 # ALLOWED_HOSTS = [
 #     "DJANGO_ALLOWED_HOSTS",
